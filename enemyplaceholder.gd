e3579a9b22ae0440c5e55304e5b096a6acca8beb
Magacu <melendomorado@gmail.com>
2025-01-24 23:25:07 +0100

    Assigment of sprites and messages

--- enemyplaceholder.gd
+++ enemyplaceholder.gd
@@ -1,10 +1,18 @@
 extends RigidBody2D
 
 @export var type: int
+var sprite:Sprite2D
+var label:Label
+var sprites=["res://pan.png","res://tran.png"]
+var messages=[["pan1","pan2"],["tran1","tran2"]]
+var rng =RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	sprite = get_node("Sprite2D2")
+	sprite.texture=load(sprites[type])
+	label = get_node("Label")
+	label.text = messages[type][randi_range(0,1)]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
