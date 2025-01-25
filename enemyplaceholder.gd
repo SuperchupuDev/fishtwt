1607a98215ef02fa351a75c2feb14a5d30844296
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2025-01-25 14:39:55 +0100

    speed increaser

--- enemyplaceholder.gd
+++ enemyplaceholder.gd
@@ -5,7 +5,6 @@ var sprite:Sprite2D
 var label:Label
 var sprites=["res://fishpfp1.png","res://baitpfp1.png"]
 var messages=[["pan1","pan2"],["tran1","tran2"]]
-var rng =RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,18 +12,23 @@ func _ready() -> void:
 	sprite = get_node("Sprite2D2")
 	sprite.texture=load(sprites[type])
 	label = get_node("Label")
-	label.text = messages[type][randi_range(0,1)]
+	label.text = messages[type][randi() % 2]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-func _input_event(viewport, event:InputEvent, shape_idx):
+func _input_event(_viewport, event: InputEvent, _shape_idx):
 	if  event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var old_points = Global.points
 		if type == 0:
 			Global.points += 1
 		else:
 			Global.points += 5
+		if old_points < 10 and Global.points >= 10:
+			Global.speed += 1
+		elif old_points < 30 and Global.points >= 30:
+			Global.speed += 1
 		print("clicked");
 		queue_free()
